73b269b40f289857e0d503c7890f1a05c2463394
AbelPerezF <65930136+CainFernandez@users.noreply.github.com>
2024-03-31 15:38:48 -0500

    Inclusión de contraseña encriptada.

--- Index.php
+++ Index.php
@@ -13,9 +13,9 @@
             }else{
                 require_once "conexion.php";
 
-                $user = $_POST['usuario'];
-                $pass = $_POST['clave'];
-
+                $user = mysqli_real_escape_string($conection,$_POST['usuario']);
+                $pass = md5(mysqli_real_escape_string($conection,$_POST['clave']));
+                
                 $query = mysqli_query($conection, "SELECT * FROM usuario WHERE usuario= '$user' AND clave = '$pass'");
                 $result = mysqli_num_rows($query);
 
