29e69e643ea57fc7302330927e5764ffafec130b
AbelPerezF <65930136+CainFernandez@users.noreply.github.com>
2024-03-31 12:34:18 -0500

    Vista login en php

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Sistema Facturación</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section id="container">
+        <form action="" method="post">
+            <h3>Iniciar Sesión</h3>
+            <img src="img/logi.png" alt="Login">
+            <input type="text" name="usuario" placeholder="Usuario">
+            <input type="password" name="clave" placeholder="Contraseña">
+            <p class="alert">Mensaje</p>
+            <input type="submit" value="INGRESAR">
+        </form>
+    </section>
+</body>
+</html>
