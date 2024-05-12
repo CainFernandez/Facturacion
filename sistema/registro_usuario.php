64346b7f8ca78531c9c8816a3917d459258bb8c9
AbelPerezF <65930136+CainFernandez@users.noreply.github.com>
2024-05-12 15:16:03 -0500

    Creación de controlador  de registro usuario

--- sistema/registro_usuario.php
+++ sistema/registro_usuario.php
@@ -1,3 +1,40 @@
+<?php 
+
+    include "../conexion.php";
+    $alert='';
+    if (empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['usuario'])
+    || empty($_POST['clave']) || empty($_POST['rol'])) 
+    {
+        $alert='<p class="msg_error">Todo los campos son obligatorios.</p>';
+    }else{
+
+        $nombre = $_POST['nombre'];
+        $email = $_POST['correo'];
+        $user = $_POST['usuario'];
+        $clave = md5($_POST['clave']);
+        $rol = $_POST['rol'];
+
+        $query = mysqli_query($conection,"SELECT * FROM usuario WHERE usuario = '$user' OR correo = '$email'");
+        $result = mysqli_fetch_array($query);
+        
+        if ($result > 0) {
+            $alert = '<p class="msg_error">El correo o el usuario ya existe.</p>';
+        } else {
+            $query_insert = mysqli_query($conection,"INSERT INTO usuario(nombre,correo,usuario,clave,rol)
+                                                                VALUES('$nombre','$email','$user','$clave','$rol')");
+            
+            if ($query_insert) {
+                $alert = '<p class="msg_save">Usuario creado correctamente.</p>';
+            } else {
+                $alert = '<p class="msg_error">Error al crear el usuario.</p>';
+            }
+            
+        }
+        
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,9 +50,9 @@
 		<div class="form_register">
             <h1>Registro usuario</h1>
             <hr>
-            <div class="alert"></div>
+            <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
 
-            <form>
+            <form action="" method="post">
                 <label for="nombre">Nombre</label>
                 <input type="text" name="nombre" id="nombre" placeholder="Nombre completo">
                 <label for="correo">Correo electrónico</label>
@@ -25,10 +62,21 @@
                 <label for="clave">Clave</label>
                 <input type="password" name="clave" id="clave" placeholder="Clave de acceso">
                 <label for="rol">Tipo Usuario</label>
+                <?php 
+                    $query_rol = mysqli_query($conection, "SELECT * FROM rol");
+                    $result_rol = mysqli_num_rows($query_rol);
+                ?>
                 <select name="rol" id="rol">
-                    <option value="1">Administrador</option>
-                    <option value="2">Supervisor</option>
-                    <option value="3">Vendedor</option>
+                    <?php
+                        if ($result_rol > 0) 
+                        {
+                          while($rol = mysqli_fetch_array($query_rol)){
+                    ?>
+                        <option value="<?php echo $rol["idrol"]; ?>"><?php echo $rol["rol"]; ?></option>
+                    <?php  
+                          }
+                        }
+                    ?>
                 </select>
                 <input type="submit" value="Crear usuario" class="btn_save">
                 
