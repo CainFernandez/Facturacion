ec36add740c3c43a60feec052f9441b052a9fdfb
AbelPerezF <65930136+CainFernandez@users.noreply.github.com>
2024-08-30 22:15:01 -0500

    Creación del PAGINADOR.

--- sistema/lista_usuarios.php
+++ sistema/lista_usuarios.php
@@ -29,8 +29,25 @@
                 <th>Acciones</th>
             </tr>
             <?php
+                //PAGINADOR
+                $sql_registe = mysqli_query($conection, "SELECT COUNT(*) as total_registro FROM usuario WHERE estatus = 1");
+                $result_register = mysqli_fetch_array($sql_registe);
+                $total_registro = $result_register['total_registro'];
+
+                $por_pagina = 5;
+
+                if(empty($_GET['pagina']))
+                {
+                    $pagina = 1;
+                }else{
+                    $pagina = $_GET['pagina'];
+                }
+
+                $desde = ($pagina-1) * $por_pagina;
+                $total_paginas = ceil($total_registro / $por_pagina);
+
                 $query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM
-                usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1 ORDER BY idusuario ASC");
+                usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1 ORDER BY u.idusuario ASC LIMIT $desde,$por_pagina");
 
                 $result = mysqli_num_rows($query);
                 if ($result > 0) {
@@ -57,6 +74,35 @@
                 }
             ?>
         </table>
+        <div class="paginador">
+            <ul>
+                <?php
+                    if($pagina != 1)
+                    { 
+                ?>
+                    <li><a href="?pagina=<?php echo 1; ?>">|<</a></li>
+                    <li><a href="?pagina=<?php echo $pagina-1; ?>"><<</a></li>
+
+                <?php
+                    }
+                    for ($i=1; $i <= $total_paginas; $i++) { 
+                        # code...
+                        if($i == $pagina) {
+                            echo '<li class="pageSelected">'.$i.'</li>';
+                        } else {
+                            echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
+                        }
+                    }
+
+                    if($pagina != $total_paginas)
+                    {
+                ?>
+                        <li><a href="?pagina=<?php echo $pagina + 1; ?>">>></a></li>
+                        <li><a href="?pagina=<?php echo $total_paginas; ?>">>|</a></li>
+                <?php } ?>
+            
+            </ul>
+        </div>
 
 	</section>
 
