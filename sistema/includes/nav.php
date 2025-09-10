bc3f92a0f823285289069285e9f0e5dc9f56e5fe
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-09-10 11:41:46 -0500

    Registro Proveedor

--- sistema/includes/nav.php
+++ sistema/includes/nav.php
@@ -19,13 +19,17 @@
 						    <li><a href="lista_clientes.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Lista de Clientes</a></li>
 					    </ul>
 				    </li>
-				    <li class="principal">
-					    <a href="#"><i class="fa fa-building" aria-hidden="true"></i> Proveedores</a>
-					    <ul>
-						    <li><a href="#"><i class="fa fa-plus-square" aria-hidden="true"></i> Nuevo Proveedor</a></li>
-						    <li><a href="#"><i class="fa fa-th-list" aria-hidden="true"></i> Lista de Proveedores</a></li>
-					</ul>
-				    </li>
+					<?php 
+				        if ($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2) {
+					?>
+				        <li class="principal">
+					        <a href="#"><i class="fa fa-building" aria-hidden="true"></i> Proveedores</a>
+					        <ul>
+						        <li><a href="registro_proveedor.php"><i class="fa fa-plus-square" aria-hidden="true"></i> Nuevo Proveedor</a></li>
+						        <li><a href="#"><i class="fa fa-th-list" aria-hidden="true"></i> Lista de Proveedores</a></li>
+					        </ul>
+				        </li>
+					<?php } ?>
 				    <li class="principal">
 					    <a href="#"><i class="fa fa-cubes" aria-hidden="true"></i> Productos</a>
 					    <ul>
