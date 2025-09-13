ee158380d59ccf2fa79cd055bccb690e224586c8
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-09-13 10:03:53 -0500

    Create eliminar_confirmar_proveedor.php

---
+++ sistema/eliminar_confirmar_proveedor.php
@@ -0,0 +1,87 @@
+<?php
+
+    session_start();
+    if ($_SESSION['rol'] != 1 && $_SESSION['rol'] != 2) 
+    {
+	    header("location: ./");
+    }
+    
+	include "../conexion.php";
+
+    if(!empty($_POST))
+	{
+        if(empty($_POST['idproveedor']))
+        {
+            header("location: lista_proveedor.php");
+            mysqli_close($conection);
+        }
+
+		$idproveedor = $_POST['idproveedor'];
+
+		$query_delete = mysqli_query($conection, "UPDATE proveedor SET estatus = 0 WHERE codproveedor = $idproveedor");
+		mysqli_close($conection);
+
+		if($query_delete){
+			header("location: lista_proveedor.php");
+		}else{
+			echo "Error al eliminar";
+		}
+	}
+
+
+    //Consultar a BD y mostrar datos.
+    if(empty($_REQUEST['id'])) 
+	{
+		header("location: lista_proveedor.php");
+		mysqli_close($conection);
+
+	} else {
+
+		$idproveedor = $_REQUEST['id'];
+
+		$query = mysqli_query($conection, "SELECT * FROM proveedor WHERE codproveedor = $idproveedor ");
+		mysqli_close($conection);
+
+		$result = mysqli_num_rows($query);
+
+		if ($result > 0 ) {
+			while($data = mysqli_fetch_array($query)){
+
+				$proveedor = $data['proveedor'];
+			}
+		} else {
+			header("location: lista_proveedor.php");
+		}
+		
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<?php include "includes/scripts.php"; ?>
+	<title>Eliminar Proveedor</title>
+</head>
+<body>
+	<?php include "includes/header.php"; ?>
+	
+	<section id="container">
+		<div class="data_delete">
+            <i class="fa fa-building fa-7x" aria-hidden="true" style="color: #e66262"></i>
+			<br>
+			<br>
+			<h2>¿Está seguro de eliminar el siguiente registro?</h2>
+			<p>Nombre del proveedor: <span><?php echo $proveedor; ?></span></p>
+			
+			<form method="post" action="" >
+                <input type="hidden" name="idproveedor" value="<?php echo $idproveedor; ?>">
+				<a href="lista_proveedor.php" class="btn_cancel"><i class="fa fa-ban" aria-hidden="true"></i> Cancelar</a>
+				<button type="submit" class="btn_ok"><i class="fa fa-trash-alt" aria-hidden="true"></i> Eliminar</button>
+			</form>
+		</div>
+	</section>
+
+	<?php include "includes/footer.php"; ?>
+</body>
+</html>
