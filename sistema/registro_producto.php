2c6fddf45a6ad890ebf4fce9a73f8bf581f02409
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-09-21 13:00:47 -0500

    POST - Registro Producto

--- sistema/registro_producto.php
+++ sistema/registro_producto.php
@@ -10,26 +10,47 @@
     include "../conexion.php";
 
     if (!empty($_POST)) {
+        
         $alert='';
-        if (empty($_POST['proveedor']) || empty($_POST['contacto'])  || empty($_POST['telefono']) || empty($_POST['direccion'])) 
+        if (empty($_POST['proveedor']) || empty($_POST['producto'])  || empty($_POST['precio']) || $_POST['precio'] <= 0 
+        || empty($_POST['cantidad']) || $_POST['cantidad'] <= 0) 
         {
             $alert='<p class="msg_error">Todo los campos son obligatorios.</p>';
         }else{
 
             $proveedor  = $_POST['proveedor'];
-            $contacto   = $_POST['contacto'];
-            $telefono   = $_POST['telefono'];
-            $direccion  = $_POST['direccion']; 
+            $producto   = $_POST['producto'];
+            $precio     = $_POST['precio'];
+            $cantidad   = $_POST['cantidad']; 
             $usuario_id = $_SESSION['idUser'];
 
-                $query_insert = mysqli_query($conection,"INSERT INTO proveedor(proveedor,contacto,telefono,direccion,usuario_id)
-                                                                     VALUES('$proveedor','$contacto','$telefono','$direccion','$usuario_id')");
+            //POST - FOTO
+            $foto        = $_FILES['foto'];
+            $nombre_foto = $foto['name'];
+            $type        = $foto['type'];
+            $url_temp    = $foto['tmp_name'];
+
+            $imgProducto = 'img_producto.png';
+
+            if($nombre_foto != '')
+            {
+                $destino    = 'img/uploads/';
+                $img_nombre = 'img_'.md5(date('d-m-Y H:m:s'));
+                $imgProducto = $img_nombre.'.jpg';
+                $src         = $destino.$imgProducto;
+            }
+
+            $query_insert = mysqli_query($conection,"INSERT INTO producto(proveedor,descripcion,precio,existencia,usuario_id,foto)
+                                                                     VALUES('$proveedor','$producto','$precio','$cantidad','$usuario_id','$imgProducto')");
                 
-                if ($query_insert) {
-                    $alert = '<p class="msg_save">Cliente guardado correctamente.</p>';
-                } else {
-                    $alert = '<p class="msg_error">Error al guardar el cliente.</p>';
-                }                                                        
+            if ($query_insert) {
+                if($nombre_foto != ''){
+                    move_uploaded_file($url_temp,$src);
+                }
+                $alert = '<p class="msg_save">Producto guardado correctamente.</p>';
+            } else {
+                $alert = '<p class="msg_error">Error al guardar el producto.</p>';
+            }                                                        
             
         }
     }
