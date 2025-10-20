a590d1f058a2ce52e2aaaef71d549a28358e2eea
CainP <65930136+CainFernandez@users.noreply.github.com>
2025-10-20 09:14:45 -0500

    Procesar la venta

--- sistema/ajax.php
+++ sistema/ajax.php
@@ -386,6 +386,38 @@
                 exit;
             }
 
+            //Accion procesarVenta para venta.
+            if($_POST['action'] == 'procesarVenta'){
+                
+                if(empty($_POST['codcliente'])){
+                    $codcliente = 1;
+                }else{
+                    $codcliente = $_POST['codcliente']; 
+                }
+
+                $token   = md5($_SESSION['idUser']);
+                $usuario = $_SESSION['idUser'];
+
+                $query = mysqli_query($conection, "SELECT * FROM detalle_temp WHERE token_user = '$token'");
+                $result = mysqli_num_rows($query);
+
+                if($result > 0)
+                {
+                    $query_procesar = mysqli_query($conection, "CALL procesar_venta($usuario,$codcliente,'$token')");
+                    $result_detalle = mysqli_num_rows($query_procesar);
+
+                    if($result_detalle > 0){
+                        $data = mysqli_fetch_assoc($query_procesar);
+                        echo json_encode($data,JSON_UNESCAPED_UNICODE);
+                    }else{
+                        echo "error";
+                    }
+                }else{
+                    echo "error";
+                }
+                mysqli_close($conection);
+                exit;
+            }
         //---------END NUEVA VENTA.
 
     }
